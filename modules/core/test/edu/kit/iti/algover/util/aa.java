f736e0e459792d213104bc467b146b5c831e7c61
Mattias Ulbrich <ulbrich@kit.edu>
2016-08-03 20:03:41 +0200

    moving source files to modules

---
+++ modules/core/test/edu/kit/iti/algover/util/aa.java
@@ -0,0 +1,14 @@
+package edu.kit.iti.algover.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class aa {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
