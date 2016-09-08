bbf983ddc73b99e551686a932e7b18695c4a08ef
azishi <azadeh.shirvanian@gmail.com>
2016-09-08 15:32:56 +0200

    [test] added MainWindow class in GUI package

---
+++ modules/gui/src/edu/kit/iti/algover/gui/MainWindow.java
@@ -0,0 +1,13 @@
+package edu.kit.iti.algover.gui;
+
+import javax.swing.*;
+
+/**
+ * MainWindow class will show mainwindow
+ * Created by sony on 08.09.2016.
+ */
+public class MainWindow extends JFrame {
+    public MainWindow(){
+        super();
+    }
+}
