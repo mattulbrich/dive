26c2a4cc6985f72d6b74521bc3551554321f6a32
azishi <azadeh.shirvanian@gmail.com>
2016-09-13 21:49:54 +0200

    [interim] added a footer panel to the editor window.

---
+++ modules/gui/src/edu/kit/iti/algover/gui/FooterPanel.java
@@ -0,0 +1,32 @@
+package edu.kit.iti.algover.gui;
+
+import net.miginfocom.swing.MigLayout;
+import sun.management.snmp.jvmmib.EnumJvmClassesVerboseLevel;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Footer panel contains two labels at right and left.
+ *
+ * Created by Azadeh Shirvanian on 13.09.2016.
+ */
+public class FooterPanel extends JPanel {
+
+    JLabel leftLabel = new JLabel("Left");
+    JLabel rightLabel = new JLabel("Line");
+
+    MigLayout migLayout = new MigLayout(
+            "insets 0 5 0 5",       //Layout constraints
+            "[grow][grow]",               // Column constraints
+            ""          // Row constraints
+    );
+
+    public FooterPanel(){
+
+        setLayout(migLayout);
+
+        add(leftLabel, "align left");
+        add(rightLabel, "align right");
+    }
+}
