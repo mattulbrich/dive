a056d8d3344ac3f0f86474b2428e056d4e6071c1
azishi <azadeh.shirvanian@gmail.com>
2016-11-07 10:26:18 +0100

    [interim] added SettingsZ3BrowseAction

---
+++ modules/gui/src/edu/kit/iti/algover/Actions/SettingsZ3BrowseAction.java
@@ -0,0 +1,57 @@
+package edu.kit.iti.algover.Actions;
+
+import edu.kit.iti.algover.gui.GUICenter;
+import edu.kit.iti.algover.gui.SettingsPanelZ3;
+import edu.kit.iti.algover.gui.SettingsWindow;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.io.File;
+
+/**
+ * Created by Azadeh Shirvanian on 04.11.2016.
+ */
+public class SettingsZ3BrowseAction extends AbstractAction {
+
+    GUICenter c;
+    SettingsWindow settingsWindow;
+    //JPanel cards;
+    SettingsPanelZ3 settingsPanelZ3;
+    JTextField pathTextField;
+
+    public SettingsZ3BrowseAction(GUICenter center){
+
+        this.c = center;
+        settingsWindow = c.getSettingsWindow();
+        settingsPanelZ3 = c.getSettingsPanelZ3();
+      //  this.cards = settingsWindow.getCards();
+        //this.settingsPanelZ3 = (SettingsPanelZ3) cards.getComponent(1);
+
+        //Component SettingsMainPanel = cards.getComponent(1);
+        //settingsPanelZ3 = (SettingsPanelZ3) SettingsMainPanel;
+       // this.pathTextField = settingsPanelZ3.getPathText();
+
+        if (settingsWindow == null)
+            System.out.println("Window is null");
+
+        this.putValue(AbstractAction.NAME, "...");
+        //this.putValue(AbstractAction.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.CTRL_MASK));
+        //this.putValue(AbstractAction.MNEMONIC_KEY, KeyEvent.VK_O);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        JFileChooser fc = new JFileChooser();
+        fc.setDialogTitle("Browse for File");
+        int result = fc.showDialog(settingsWindow, "OK");
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fc.getSelectedFile();
+          //  pathTextField.setText(selectedFile.getAbsolutePath());
+            System.out.println("File: " + selectedFile.getAbsolutePath());
+        }
+
+    }
+}
