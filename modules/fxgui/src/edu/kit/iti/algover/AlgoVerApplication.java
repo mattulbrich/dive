536189e43c921794068a0f97e330dd7431ba060f
Philipp Krüger <matheus.dev@gmail.com>
2017-06-15 14:15:10 +0200

    Test module dependencies.
    Hint: They work

--- modules/fxgui/src/edu/kit/iti/algover/AlgoVerApplication.java
+++ modules/fxgui/src/edu/kit/iti/algover/AlgoVerApplication.java
@@ -1,9 +1,15 @@
 package edu.kit.iti.algover;
 
+import edu.kit.iti.algover.facade.ProjectFacade;
+import edu.kit.iti.algover.project.Project;
+import edu.kit.iti.algover.util.Debug;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 /**
  * Created by Philipp on 15.06.2017.
@@ -12,7 +18,20 @@ public class AlgoVerApplication extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    primaryStage.setScene(new Scene(new TextField("HI")));
+    // Setup view
+    TextArea area = new TextArea("...");
+    primaryStage.setScene(new Scene(area));
     primaryStage.show();
+
+    // Let user choose a project directory
+    DirectoryChooser chooser = new DirectoryChooser();
+    chooser.setTitle("Choose project");
+    chooser.setInitialDirectory(new File("modules/core/test-res/edu/kit/iti/algover"));
+    File projectDir = chooser.showDialog(primaryStage);
+
+    // Read all PVCs and update GUI
+    Project project = ProjectFacade.getInstance().buildProject(projectDir);
+    CharSequence seq = Debug.toString(project.getAllVerificationConditions());
+    area.setText(seq.toString());
   }
 }
