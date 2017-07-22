a8cb25f0d87ab3cce4864080b0063a2eca10dc73
Philipp Krüger <matheus.dev@gmail.com>
2017-07-22 22:03:53 +0200

    Explore a RichTextFX-based approach

--- modules/fxgui/src/edu/kit/iti/algover/sequent/TermView.java
+++ modules/fxgui/src/edu/kit/iti/algover/sequent/TermView.java
@@ -3,34 +3,47 @@ package edu.kit.iti.algover.sequent;
 import edu.kit.iti.algover.prettyprint.AnnotatedString;
 import edu.kit.iti.algover.prettyprint.PrettyPrint;
 import edu.kit.iti.algover.term.Term;
-import edu.kit.iti.algover.util.Span;
-import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import org.fxmisc.richtext.CharacterHit;
+import org.fxmisc.richtext.CodeArea;
+
+import java.util.OptionalInt;
 
 /**
  * Created by Philipp on 22.07.2017.
  */
-public class TermView extends HighlightableLabel {
+public class TermView extends CodeArea {
 
     private final Term term;
+    private final AnnotatedString str;
 
     public TermView(Term term) {
         super("");
         this.term = term;
-        getStyleClass().add("term-view");
-        updateText(term);
-        setOnMouseClicked(this::handleClick);
-    }
-
-    private void handleClick(MouseEvent mouseEvent) {
-        System.out.println(hit(mouseEvent.getX(), mouseEvent.getY()));
-    }
-
-    private void updateText(Term term) {
         PrettyPrint p = new PrettyPrint();
         p.setPrintingFix(false);
-        AnnotatedString prettyPrinted = p.print(term, 10);
-        setText(prettyPrinted.toString());
-        //setHighlightedSpan(new Span(1, 2, 4, 6));
+        this.str = p.print(term, 80);
+
+        replaceText(0, getLength(), str.toString());
+
+        getStyleClass().add("term-view");
+        setEditable(false);
+
+        setOnMouseMoved(this::updateHighlghting);
+        setOnMousePressed(this::updateHighlghting);
+        setOnMouseExited(event -> clearStyle(0, getLength()));
+    }
+
+    private void updateHighlghting(MouseEvent mouseEvent) {
+        CharacterHit hit = hit(mouseEvent.getX(), mouseEvent.getY());
+        OptionalInt charIdx = hit.getCharacterIndex();
+        if (charIdx.isPresent()) {
+            AnnotatedString.TermElement elem = str.getTermElementAt(charIdx.getAsInt());
+            clearStyle(0, getLength());
+            setStyleClass(elem.getBegin(), elem.getEnd(), "highlighted");
+            mouseEvent.consume();
+        } else {
+            clearStyle(0, getLength());
+        }
     }
 }
