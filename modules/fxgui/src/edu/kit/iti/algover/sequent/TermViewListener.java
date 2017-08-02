217115d26cbe1a1f094e986325187b885f3a2e35
Philipp Krüger <matheus.dev@gmail.com>
2017-08-02 13:29:05 +0200

    Make TermCells focusable on click next to their text.
    So the ListView that shows them gets their item selected, if you click next to the text,
    instead of the CodeAreas themselves trying to get focused.

---
+++ modules/fxgui/src/edu/kit/iti/algover/sequent/TermViewListener.java
@@ -0,0 +1,14 @@
+package edu.kit.iti.algover.sequent;
+
+import edu.kit.iti.algover.term.Term;
+import edu.kit.iti.algover.term.prettyprint.SubtermSelector;
+
+/**
+ * Created by philipp on 02.08.17.
+ */
+public interface TermViewListener {
+    void handleClickOnSubterm(Term term, SubtermSelector subtermSelector);
+
+    void handleClickOutsideTerm();
+
+}
