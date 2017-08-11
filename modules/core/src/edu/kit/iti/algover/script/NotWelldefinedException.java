f36437d6689ddaff4bb167165d5172ef357da223
Sarah Grebing <grebing@ira.uka.de>
2017-08-11 16:09:14 +0200

    ScriptParser and Interpreter copy paste from scriptdebugger, needs revision

---
+++ modules/core/src/edu/kit/iti/algover/script/NotWelldefinedException.java
@@ -0,0 +1,43 @@
+package edu.kit.formal.proofscriptparser;
+
+/*-
+ * #%L
+ * ProofScriptParser
+ * %%
+ * Copyright (C) 2017 Application-oriented Formal Verification
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+
+import edu.kit.formal.proofscriptparser.ast.Expression;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author Alexander Weigl
+ * @version 1 (01.05.17)
+ */
+@Data
+@AllArgsConstructor
+public class NotWelldefinedException extends Exception {
+    private final Expression expr;
+
+    public NotWelldefinedException(String message, Expression e) {
+        super(message);
+        this.expr = e;
+    }
+}
