52210dc1d2dd6ac1ab2a390ab1970a28a5324486
Philipp Krüger <matheus.dev@gmail.com>
2017-09-19 14:57:44 +0200

    Use icon instead of "branches" string in rule application overlay

--- modules/fxgui/src/edu/kit/iti/algover/rule/RuleViewOverlay.java
+++ modules/fxgui/src/edu/kit/iti/algover/rule/RuleViewOverlay.java
@@ -1,9 +1,15 @@
 package edu.kit.iti.algover.rule;
 
+import de.jensd.fx.glyphs.GlyphsDude;
+import de.jensd.fx.glyphs.materialicons.MaterialIcon;
 import edu.kit.iti.algover.rules.ProofRuleApplication;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import org.controlsfx.glyphfont.Glyph;
+
 
 public class RuleViewOverlay extends AnchorPane {
     private final ProofRuleApplication application;
@@ -13,15 +19,10 @@ public class RuleViewOverlay extends AnchorPane {
 
         int count = application.getBranchCount();
 
-        Label branchCount = new Label(count + fromPlural(count, " branch", " branches"));
+        Label branchCount = new Label(count + "", GlyphsDude.createIcon(MaterialIcon.CALL_SPLIT, "20px"));
         branchCount.setAlignment(Pos.TOP_RIGHT);
         branchCount.getStyleClass().add("branch-count");
         setRightAnchor(branchCount, 4.0);
         getChildren().add(branchCount);
-    }
-
-    private static String fromPlural(int count, String singular, String plural) {
-        if (count == 1) return singular;
-        else return plural;
     }
 }
