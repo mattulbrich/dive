c74937f8840dc9419301f98b6ef9adce3d6b8711
Jonas Klamroth <jonas.klamroth@gmail.com>
2018-01-29 11:46:14 +0100

    basic dafnyrule implementation (waiting for schemavarmatcher

--- modules/core/src/edu/kit/iti/algover/rules/impl/DafnyRule.java
+++ modules/core/src/edu/kit/iti/algover/rules/impl/DafnyRule.java
@@ -6,22 +6,23 @@ import edu.kit.iti.algover.data.BuiltinSymbols;
 import edu.kit.iti.algover.data.MapSymbolTable;
 import edu.kit.iti.algover.data.SymbolTable;
 import edu.kit.iti.algover.parser.*;
+import edu.kit.iti.algover.proof.ProofFormula;
 import edu.kit.iti.algover.proof.ProofNode;
 import edu.kit.iti.algover.rules.*;
-import edu.kit.iti.algover.script.interpreter.TermMatcher;
+import edu.kit.iti.algover.term.builder.ReplacementVisitor;
+//import edu.kit.iti.algover.term.match.Matching;
+//import edu.kit.iti.algover.term.match.TermMatcher;
 import edu.kit.iti.algover.term.*;
 import edu.kit.iti.algover.term.builder.TermBuildException;
 import edu.kit.iti.algover.term.builder.TermBuilder;
 import edu.kit.iti.algover.term.builder.TreeTermTranslator;
-import edu.kit.iti.algover.term.parser.TermParser;
+import edu.kit.iti.algover.util.ImmutableList;
 import edu.kit.iti.algover.util.RuleUtil;
+
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by jklamroth on 1/25/18.
@@ -30,6 +31,7 @@ public class DafnyRule extends AbstractProofRule {
     private String name;
     private String fileName;
     private SymbolTable symbolTable;
+    private List<String> schemaVars;
     private DafnyTree tree;
     private Term searchTerm;
     private Term replaceTerm;
@@ -73,23 +75,13 @@ public class DafnyRule extends AbstractProofRule {
         TreeTermTranslator ttt = new TreeTermTranslator(symbolTable);
         try {
             searchTerm = ttt.build(equalsClause.getChild(0));
+            System.out.println(searchTerm);
+            searchTerm = searchTerm.accept(new ReplaceProgrammVariableVisitor(), schemaVars);
+            System.out.println(searchTerm);
             replaceTerm = ttt.build(equalsClause.getChild(1));
         } catch (TermBuildException e) {
             System.out.println("Error parsing equalsClause");
         }
-    }
-
-    private Term replaceVarsWithSchemaVars(Term t) {
-        if(t instanceof VariableTerm) {
-            VariableTerm vt = (VariableTerm)t;
-            SchemaVarTerm svt = new SchemaVarTerm(vt.getName(), vt.getSort());
-            return svt;
-        }
-        for(Term term : t.getSubterms()) {
-            TermBuilder tb = new TermBuilder(symbolTable);
-
-        }
-        return null;
     }
 
     /**
@@ -100,11 +92,13 @@ public class DafnyRule extends AbstractProofRule {
     private SymbolTable makeSymbolTable() {
 
         Collection<FunctionSymbol> map = new ArrayList<>();
+        schemaVars = new ArrayList<>();
 
         for (DafnyTree decl : ProgramDatabase.getAllVariableDeclarations(tree)) {
             String name = decl.getChild(0).toString();
             Sort sort = treeToType(decl.getChild(1));
             map.add(new FunctionSymbol(name, sort));
+            schemaVars.add(name);
         }
 
         MapSymbolTable st = new MapSymbolTable(new BuiltinSymbols(), map);
@@ -126,11 +120,69 @@ public class DafnyRule extends AbstractProofRule {
 
     @Override
     public ProofRuleApplication considerApplication(ProofNode target, Sequent selection, TermSelector selector) throws RuleException {
+        /*Term selected = selector.selectTopterm(target.getSequent()).getTerm();
+        TermMatcher tm = new TermMatcher();
+        ImmutableList<Matching> matchings = tm.match(searchTerm, selected);
+        if(matchings.size() == 0) {
+            return ProofRuleApplicationBuilder.notApplicable(this);
+        }
+        ProofRuleApplicationBuilder proofRuleApplicationBuilder;
+        try {
+            replaceTerm = matchings.get(0).instantiate(replaceTerm);
+            proofRuleApplicationBuilder = new ProofRuleApplicationBuilder(this);
+            proofRuleApplicationBuilder.setApplicability(ProofRuleApplication.Applicability.APPLICABLE);
+            proofRuleApplicationBuilder.setTranscript(getTranscript());
+            proofRuleApplicationBuilder.newBranch().addReplacement(selector, replaceTerm);
+        } catch (TermBuildException e) {
+            throw new RuleException();
+        }
+
+        return proofRuleApplicationBuilder.build();*/
         return null;
     }
 
     @Override
     public ProofRuleApplication makeApplication(ProofNode target, Parameters parameters) throws RuleException {
+        /*Term on = parameters.getValue("on").cast(Term.class).getValue();
+        TermMatcher tm = new TermMatcher();
+        ImmutableList<Matching> matchings = tm.match(searchTerm, on);
+        if(matchings.size() == 0) {
+            throw new RuleException();
+        }
+
+        ProofRuleApplicationBuilder proofRuleApplicationBuilder;
+        try {
+            replaceTerm = matchings.get(0).instantiate(replaceTerm);
+            proofRuleApplicationBuilder = new ProofRuleApplicationBuilder(this);
+            if(Optional.empty().equals(RuleUtil.matchTopLevelInAntedecent(on::equals, target.getSequent()))) {
+                proofRuleApplicationBuilder.newBranch().addDeletionsSuccedent(new ProofFormula(on)).
+                        addAdditionsSuccedent(new ProofFormula(replaceTerm));
+            } else {
+                proofRuleApplicationBuilder.newBranch().addDeletionsAntecedent(Collections.singletonList(new ProofFormula(on))).
+                        addAdditionAntecedent(new ProofFormula(replaceTerm));
+            }
+        } catch (TermBuildException e) {
+            throw new RuleException();
+        }
+
+        return proofRuleApplicationBuilder.build();*/
         return null;
+    }
+
+    private String getTranscript() {
+        String res = name;
+        return name;
+    }
+}
+
+class ReplaceProgrammVariableVisitor extends ReplacementVisitor<List<String>> {
+
+    @Override
+    public Term visit(ApplTerm applTerm, List<String> programmVariables) throws TermBuildException {
+        if(programmVariables.contains(applTerm.getFunctionSymbol().getName())) {
+            return new SchemaVarTerm("?" + applTerm.getFunctionSymbol().getName(), applTerm.getSort());
+        } else {
+            return super.visit(applTerm, programmVariables);
+        }
     }
 }
