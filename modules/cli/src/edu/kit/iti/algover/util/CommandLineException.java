0e089fb16dbf4a13fff3c896c27f1ed3cdd56283
Mattias Ulbrich <ulbrich@kit.edu>
2018-01-31 16:49:46 +0100

    introducing command line interface

---
+++ modules/cli/src/edu/kit/iti/algover/util/CommandLineException.java
@@ -0,0 +1,64 @@
+/*
+ * This file is part of AlgoVer.
+ *
+ * Copyright (C) 2015-2018 Karlsruhe Institute of Technology
+ */
+/*
+ * This file is part of
+ *    ivil - Interactive Verification on Intermediate Language
+ *
+ * Copyright (C) 2009-2012 Karlsruhe Institute of Technology
+ *
+ * The system is protected by the GNU General Public License.
+ * See LICENSE.TXT (distributed with this file) for details.
+ */
+package edu.kit.iti.algover.util;
+
+/**
+ * Exception used by {@link CommandLine}.
+ *
+ * @see CommandLine
+ */
+@SuppressWarnings("serial")
+public class CommandLineException extends Exception {
+
+    /**
+     * Instantiates a new command line exception without message.
+     */
+    public CommandLineException() {
+        super();
+    }
+
+    /**
+     * Instantiates a new command line exception.
+     *
+     * @param message
+     *            an error message
+     * @param cause
+     *            the exception causing this exception
+     */
+    public CommandLineException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Instantiates a new command line exception.
+     *
+     * @param message
+     *            an error message
+     */
+    public CommandLineException(String message) {
+        super(message);
+    }
+
+    /**
+     * Instantiates a new command line exception.
+     *
+     * @param cause
+     *            the exception causing this exception
+     */
+    public CommandLineException(Throwable cause) {
+        super(cause);
+    }
+
+}
