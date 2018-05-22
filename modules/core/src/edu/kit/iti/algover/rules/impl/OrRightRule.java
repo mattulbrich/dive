d0b9f6a552d1f2613989a29e49362f5994f4f9a8
Jonas Klamroth <jonas.klamroth@gmail.com>
2018-05-22 18:16:38 +0200

    some more basic rules

---
+++ modules/core/src/edu/kit/iti/algover/rules/impl/OrRightRule.java
@@ -0,0 +1,98 @@
+package edu.kit.iti.algover.rules.impl;
+
+import edu.kit.iti.algover.data.BuiltinSymbols;
+import edu.kit.iti.algover.proof.ProofFormula;
+import edu.kit.iti.algover.proof.ProofNode;
+import edu.kit.iti.algover.rules.*;
+import edu.kit.iti.algover.term.ApplTerm;
+import edu.kit.iti.algover.term.FunctionSymbol;
+import edu.kit.iti.algover.term.Term;
+import edu.kit.iti.algover.util.RuleUtil;
+
+import java.util.List;
+
+/**
+ * Created by jklamroth on 5/22/18.
+ */
+public class OrRightRule extends AbstractProofRule {
+    public OrRightRule() {
+        super(ON_PARAM);
+    }
+
+    @Override
+    public String getName() {
+        return "orRight";
+    }
+
+    @Override
+    protected ProofRuleApplication considerApplicationImpl(ProofNode target, Parameters parameters) throws RuleException {
+        Term on = parameters.getValue(ON_PARAM);
+        List<TermSelector> l = RuleUtil.matchSubtermsInSequent(on::equals, target.getSequent());
+        if(l.size() != 1) {
+            throw new RuleException("Matching of on parameter is ambiguous");
+        }
+        TermSelector selector = l.get(0);
+        if(!selector.isToplevel()) {
+            return ProofRuleApplicationBuilder.notApplicable(this);
+        }
+        if(!selector.isSuccedent()) {
+            return ProofRuleApplicationBuilder.notApplicable(this);
+        }
+
+        ProofFormula formula = selector.selectTopterm(target.getSequent());
+        Term term = formula.getTerm();
+        if (!(term instanceof ApplTerm)) {
+            return ProofRuleApplicationBuilder.notApplicable(this);
+        }
+        ApplTerm appl = (ApplTerm) term;
+        FunctionSymbol fs = appl.getFunctionSymbol();
+
+        if (fs != BuiltinSymbols.OR) {
+            return ProofRuleApplicationBuilder.notApplicable(this);
+        }
+
+        ProofRuleApplicationBuilder builder = handleControlParameters(parameters, target.getSequent());
+
+        builder.newBranch().addReplacement(selector, appl.getTerm(0)).
+                addAdditionsSuccedent(new ProofFormula(appl.getTerm(1)));
+        builder.setApplicability(ProofRuleApplication.Applicability.APPLICABLE);
+
+        return builder.build();
+    }
+
+    @Override
+    protected ProofRuleApplication makeApplicationImpl(ProofNode target, Parameters parameters) throws RuleException {
+        Term on = parameters.getValue(ON_PARAM);
+        List<TermSelector> l = RuleUtil.matchSubtermsInSequent(on::equals, target.getSequent());
+        if(l.size() != 1) {
+            throw new RuleException("Matching of on parameter is ambiguous.");
+        }
+        TermSelector selector = l.get(0);
+        if(!selector.isToplevel()) {
+            throw new RuleException("orRight may only be applied to TopLevel terms.");
+        }
+        if(!selector.isSuccedent()) {
+            throw new RuleException("orRight may only be applied on the succedent.");
+        }
+
+        ProofFormula formula = selector.selectTopterm(target.getSequent());
+        Term term = formula.getTerm();
+        if (!(term instanceof ApplTerm)) {
+            throw new RuleException("orRight may only be applied to or terms.");
+        }
+        ApplTerm appl = (ApplTerm) term;
+        FunctionSymbol fs = appl.getFunctionSymbol();
+
+        if (fs != BuiltinSymbols.OR) {
+            throw new RuleException("orRight may only be applied to or terms.");
+        }
+
+        ProofRuleApplicationBuilder builder = handleControlParameters(parameters, target.getSequent());
+
+        builder.newBranch().addReplacement(selector, appl.getTerm(0)).
+                addAdditionsSuccedent(new ProofFormula(appl.getTerm(1)));
+        builder.setApplicability(ProofRuleApplication.Applicability.APPLICABLE);
+
+        return builder.build();
+    }
+}
