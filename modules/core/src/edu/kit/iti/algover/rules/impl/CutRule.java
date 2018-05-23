43bb948331956e0e0fd7b3cc002e20fb5cb47f62
Jonas Klamroth <jonas.klamroth@gmail.com>
2018-05-23 15:04:35 +0200

    fixed cut rule

--- modules/core/src/edu/kit/iti/algover/rules/impl/CutRule.java
+++ modules/core/src/edu/kit/iti/algover/rules/impl/CutRule.java
@@ -5,6 +5,7 @@ import edu.kit.iti.algover.proof.ProofFormula;
 import edu.kit.iti.algover.proof.ProofNode;
 import edu.kit.iti.algover.rules.*;
 import edu.kit.iti.algover.term.ApplTerm;
+import edu.kit.iti.algover.term.Sort;
 import edu.kit.iti.algover.term.Term;
 import edu.kit.iti.algover.term.builder.TermBuildException;
 import edu.kit.iti.algover.util.RuleUtil;
@@ -36,14 +37,14 @@ public class CutRule extends AbstractProofRule {
 
     private ProofRuleApplication apply(ProofNode target, Parameters parameters) throws RuleException {
         Term with = parameters.getValue(WITH_PARAM);
+        if(with.getSort() != Sort.BOOL) {
+            throw new RuleException("Cut term has to have type bool but has type " + with.getSort() + ".");
+        }
         ProofRuleApplicationBuilder pra = new ProofRuleApplicationBuilder(this);
         pra.setApplicability(ProofRuleApplication.Applicability.APPLICABLE);
         pra.newBranch().addAdditionAntecedent(new ProofFormula(with)).setLabel("case 1");
-        try {
-            pra.newBranch().addAdditionAntecedent(new ProofFormula(new ApplTerm(BuiltinSymbols.NOT, with))).setLabel("case 2");
-        } catch (TermBuildException e) {
-            throw new RuleException("Could not create negated cut term.", e);
-        }
+        pra.newBranch().addAdditionsSuccedent(new ProofFormula(with)).setLabel("case 2");
+
         return pra.build();
     }
 }
