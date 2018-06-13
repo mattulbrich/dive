3b06a77a9283009191857481e4b2612949be86dc
Sarah Grebing <grebing@ira.uka.de>
2018-06-13 15:21:53 +0200

    Changed Parameterized Values

--- modules/core/src/edu/kit/iti/algover/script/data/Value.java
+++ modules/core/src/edu/kit/iti/algover/script/data/Value.java
@@ -9,11 +9,11 @@ import java.math.BigInteger;
  *
  * @author S.Grebing
  * @author A. Weigl
- *         //TODO alle restlichen typen ergaenzen
+ *
  */
 public class Value<T> {
-    public static final Value TRUE = new Value<>(Type.BOOL, true);
-    public static final Value FALSE = new Value<>(Type.BOOL, false);
+    public static final Value<Boolean> TRUE = new Value<>(Type.BOOL, true);
+    public static final Value<Boolean> FALSE = new Value<>(Type.BOOL, false);
 
     private final Type type;
     private final T data;
