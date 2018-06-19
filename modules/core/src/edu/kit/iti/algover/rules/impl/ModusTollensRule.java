5b7262316d3b353ed6228b12506d43e13ae4651c
Jonas Klamroth <jonas.klamroth@gmail.com>
2018-06-19 15:32:29 +0200

    unique parameter matching

--- modules/core/src/edu/kit/iti/algover/rules/impl/ModusTollensRule.java
+++ modules/core/src/edu/kit/iti/algover/rules/impl/ModusTollensRule.java
@@ -8,6 +8,9 @@ import edu.kit.iti.algover.term.ApplTerm;
 import edu.kit.iti.algover.term.FunctionSymbol;
 import edu.kit.iti.algover.term.Term;
 import edu.kit.iti.algover.term.builder.TermBuildException;
+import edu.kit.iti.algover.term.match.Matching;
+import edu.kit.iti.algover.term.match.SequentMatcher;
+import edu.kit.iti.algover.util.ImmutableList;
 import edu.kit.iti.algover.util.RuleUtil;
 
 import java.util.List;
@@ -28,12 +31,7 @@ public class ModusTollensRule extends AbstractProofRule {
 
     @Override
     protected ProofRuleApplication considerApplicationImpl(ProofNode target, Parameters parameters) throws RuleException {
-        Term on = parameters.getValue(ON_PARAM);
-        List<TermSelector> l = RuleUtil.matchSubtermsInSequent(on::equals, target.getSequent());
-        if(l.size() != 1) {
-            throw new RuleException("Matching of on parameter is ambiguous");
-        }
-        TermSelector selector = l.get(0);
+        TermSelector selector = tsForParameter.get("on");
 
         ProofFormula formula = selector.selectTopterm(target.getSequent());
         Term term = formula.getTerm();
@@ -73,12 +71,7 @@ public class ModusTollensRule extends AbstractProofRule {
 
     @Override
     protected ProofRuleApplication makeApplicationImpl(ProofNode target, Parameters parameters) throws RuleException {
-        Term on = parameters.getValue(ON_PARAM);
-        List<TermSelector> l = RuleUtil.matchSubtermsInSequent(on::equals, target.getSequent());
-        if(l.size() != 1) {
-            throw new RuleException("Matching of on parameter is ambiguous");
-        }
-        TermSelector selector = l.get(0);
+        TermSelector selector = tsForParameter.get("on");
 
         ProofFormula formula = selector.selectTopterm(target.getSequent());
         Term term = formula.getTerm();
