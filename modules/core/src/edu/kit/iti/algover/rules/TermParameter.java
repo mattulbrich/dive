79a66d134b541aabcd13bb60a39f2bb13b50931b
Jonas Klamroth <jonas.klamroth@gmail.com>
2018-09-06 14:14:55 +0200

    TermParameter Class added

---
+++ modules/core/src/edu/kit/iti/algover/rules/TermParameter.java
@@ -0,0 +1,200 @@
+package edu.kit.iti.algover.rules;
+
+import edu.kit.iti.algover.proof.ProofFormula;
+import edu.kit.iti.algover.term.SchemaCaptureTerm;
+import edu.kit.iti.algover.term.SchemaOccurTerm;
+import edu.kit.iti.algover.term.Sequent;
+import edu.kit.iti.algover.term.Term;
+import edu.kit.iti.algover.term.builder.ReplaceVisitor;
+import edu.kit.iti.algover.term.builder.TermBuildException;
+import edu.kit.iti.algover.term.match.Matching;
+import edu.kit.iti.algover.term.match.SequentMatcher;
+import edu.kit.iti.algover.term.match.TermMatcher;
+import edu.kit.iti.algover.util.ImmutableList;
+import edu.kit.iti.algover.util.RuleUtil;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by jklamroth on 9/5/18.
+ */
+public class TermParameter {
+    //Inviariant: sequent != null && (term != null || schematicTerm != null || termSelector != null)
+
+    private Term term;
+    private TermSelector termSelector;
+    private Sequent schematicTerm;
+    private Sequent sequent;
+
+    public TermParameter(Term term, Sequent sequent) {
+        this.term = term;
+        this.sequent = sequent;
+    }
+
+    public TermParameter(TermSelector termSelector, Sequent sequent) {
+        this.sequent = sequent;
+        this.termSelector = termSelector;
+    }
+
+    public TermParameter(Sequent schematicTerm, Sequent sequent) {
+        this.sequent = sequent;
+        this.schematicTerm = schematicTerm;
+    }
+
+    public Term getTerm() throws RuleException {
+        if(term != null) {
+            return term;
+        }
+        if(termSelector != null) {
+            term = termSelector.selectSubterm(sequent);
+            return term;
+        }
+        SequentMatcher sequentMatcher = new SequentMatcher();
+        ImmutableList<Matching> matchings = sequentMatcher.match(schematicTerm, sequent);
+        if(matchings.size() == 0) {
+            throw new RuleException("SchematicTerm " + schematicTerm + " does not match anything in sequent " + sequent);
+        }
+        if(matchings.size() > 1) {
+            throw new RuleException("SchematicTerm" + schematicTerm + " matches more than one term in sequent " + sequent);
+        }
+        term = matchings.get(0).get("?match").getValue();
+        return term;
+    }
+
+    public TermSelector getTermSelector() throws RuleException {
+        if(termSelector != null) {
+            return termSelector;
+        }
+        if(term != null) {
+            Optional<TermSelector> ots = RuleUtil.matchSubtermInSequent(term::equals, sequent);
+            if (ots.isPresent()) {
+                termSelector = ots.get();
+                return termSelector;
+            }
+            throw new RuleException("TermParameter: Could not find TermSelector for " + term + "in sequent " + sequent + ".");
+        }
+        SequentMatcher sequentMatcher = new SequentMatcher();
+        ImmutableList<Matching> matchings = sequentMatcher.match(schematicTerm, sequent);
+        if(matchings.size() == 0) {
+            throw new RuleException("SchematicTerm " + schematicTerm + " does not match anything in sequent " + sequent);
+        }
+        if(matchings.size() > 1) {
+            throw new RuleException("SchematicTerm" + schematicTerm + " matches more than one term in sequent " + sequent);
+        }
+        termSelector = matchings.get(0).get("?match").getTermSelector();
+        return termSelector;
+    }
+
+    public Sequent getSchematicTerm() throws RuleException {
+        if(schematicTerm != null) {
+            return schematicTerm;
+        }
+        if(termSelector != null) {
+            return getUniqueMatchingTerm(sequent, termSelector);
+        }
+        return getUniqueMatchingTerm(sequent, term);
+    }
+
+    private Sequent getUniqueMatchingTerm(Sequent sequent, Term term)  throws RuleException {
+        TermMatcher tm = new TermMatcher();
+        Optional<TermSelector> ots = RuleUtil.matchSubtermInSequent(term::equals, sequent);
+        if(ots.isPresent()) {
+            return getUniqueMatchingTerm(sequent, ots.get(), term);
+        }
+        throw new RuleException();
+    }
+
+    private Sequent getUniqueMatchingTerm(Sequent sequent, TermSelector selector) throws RuleException {
+        Term term = selector.selectSubterm(sequent);
+        return getUniqueMatchingTerm(sequent, selector, term);
+    }
+
+    /**
+     * Extracts a possibly schematic Term which is unique for the given TermSelector.
+     *
+     * @param sequent The sequent the TermSelector is related to.
+     * @param selector The selector
+     * @return a possibly schematic Term
+     * @throws RuleException if no unique matching term is available (only if 2 identical Terms in same polarity)
+     */
+    private Sequent getUniqueMatchingTerm(Sequent sequent, TermSelector selector, Term t) throws RuleException {
+        Term schemaCaptureTerm = new SchemaCaptureTerm("?match", t);
+        t = new SchemaOccurTerm(schemaCaptureTerm);
+        SequentMatcher sequentMatcher = new SequentMatcher();
+        Sequent schemaSeq;
+        if(selector.isSuccedent()) {
+            schemaSeq = new Sequent(Collections.emptyList(), Collections.singletonList(new ProofFormula(t)));
+        } else {
+            schemaSeq = new Sequent(Collections.singletonList(new ProofFormula(t)), Collections.emptyList());
+        }
+        ImmutableList<Matching> matchings = sequentMatcher.match(schemaSeq, sequent);
+        if(matchings.size() == 1) {
+            return schemaSeq;
+        }
+        TermSelector parentSelector = getParentSelector(selector);
+
+        if(parentSelector == null) {
+            matchings.forEach(matching -> System.out.println("matching = " + matching));
+            throw new RuleException("There is no unique matching term for a Parameter.");
+        }
+        return getUniqueMatchingTermRec(sequent, parentSelector,
+                selector.getPath().get(selector.getPath().size() - 1), schemaCaptureTerm);
+    }
+
+    /**
+     * Recursive version for {@link AbstractProofRule#getUniqueMatchingTerm(Sequent, TermSelector)}.
+     *
+     * @param sequent
+     * @param selector
+     * @param childIdx
+     * @param childTerm
+     * @return
+     * @throws RuleException
+     */
+    private Sequent getUniqueMatchingTermRec(Sequent sequent, TermSelector selector, int childIdx, Term childTerm) throws RuleException {
+        Term t = selector.selectSubterm(sequent);
+        Term schemaVarTerm = null;
+        try {
+            schemaVarTerm = ReplaceVisitor.replace(t, new SubtermSelector(childIdx), childTerm);
+        } catch (TermBuildException e) {
+            throw new RuleException("Error finding unique matching term.", e);
+        }
+        t = new SchemaOccurTerm(schemaVarTerm);
+        SequentMatcher sequentMatcher = new SequentMatcher();
+        Sequent schemaSeq;
+        if(selector.isSuccedent()) {
+            schemaSeq = new Sequent(Collections.emptyList(), Collections.singletonList(new ProofFormula(t)));
+        } else {
+            schemaSeq = new Sequent(Collections.singletonList(new ProofFormula(t)), Collections.emptyList());
+        }
+        ImmutableList<Matching> matchings = sequentMatcher.match(schemaSeq, sequent);
+        if(matchings.size() == 1) {
+            return schemaSeq;
+        }
+        TermSelector parentSelector = getParentSelector(selector);
+        if(parentSelector == null) {
+            throw new RuleException("There is no unique matching term for a Parameter.");
+        }
+        return getUniqueMatchingTermRec(sequent, parentSelector, selector.getPath().get(selector.getPath().size() - 1), schemaVarTerm);
+    }
+
+    /**
+     * Gets a TermSelector which points to the parent term of the term selected by the given TermSelector
+     *
+     * @param selector the selector
+     * @return the parent selector
+     */
+    private TermSelector getParentSelector(TermSelector selector) {
+        if(selector.getPath().size() <= 0) {
+            return null;
+        }
+        int intPath[] = new int[selector.getPath().size() - 1];
+        for(int i = 0; i < intPath.length; ++i) {
+            intPath[i] = selector.getPath().get(i);
+        }
+
+        return new TermSelector(selector.getPolarity(), selector.getTermNo(), intPath);
+    }
+}
