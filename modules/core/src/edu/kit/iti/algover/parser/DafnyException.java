mattulbrich/dive

commit f675e0d1f626815bc05da519d028bcd15d95ed30

improved Messages for DafnyExceptions

modules/core/src/edu/kit/iti/algover/parser/DafnyException.java
@@ -7,6 +7,8 @@
 package edu.kit.iti.algover.parser;
 
 
+import java.io.File;
+
 /**
  * The Class DafnyException is thrown by all routines that have a
  * {@link DafnyTree} at hand.
@@ -47,15 +49,14 @@ public class DafnyException extends Exception {
         return tree;
     }
 
-//    @Override
-//    public String getMessage() {
-//        String result = super.getMessage();
-//        if (tree != null) {
-//            // TODO deal with fileName == null;
-//            result += " (" + tree.getFilename() + ":" + tree.getLine()
-//                    + ":" + tree.getCharPositionInLine() + ")";
-//        }
-//        return result;
-//    }
+    @Override
+    public String getMessage() {
+        String result = "";
+        if (tree != null) {
+            result += new File(tree.getFilename()).getName() + ":" + tree.getLine()
+                    + ":" + tree.getCharPositionInLine() + super.getMessage();
+        }
+        return result;
+    }
 
 }

modules/core/src/edu/kit/iti/algover/proof/MethodPVCBuilder.java
@@ -156,6 +156,7 @@ public class MethodPVCBuilder implements PVCBuilder {
                 this.sequent =
                         sequenter.translate(pathThroughProgram, getSymbolTable(), referenceMap);
             } catch (DafnyException e) {
+            	e.getTree().setFilename(declaration.getFilename());
                 throw new RuntimeException(e);
             }
         }

modules/fxgui/src/edu/kit/iti/algover/MainController.java
@@ -352,6 +352,7 @@ public class MainController implements SequentActionListener, RuleApplicationLis
                 browserController.getView().setDisable(false);
                 sequentController.getView().setDisable(false);
                 ruleApplicationController.getView().setDisable(false);
+                manager.getProject().getDafnyFiles().forEach(df -> editorController.viewFile(df));
                 ruleApplicationController.onReset();
                 simpleStratButton.setDisable(false);
                 breadCrumbBar.setDisable(false);
@@ -365,8 +366,9 @@ public class MainController implements SequentActionListener, RuleApplicationLis
 
         //TODO somehow get proper exceptions and handling them
         t.setOnFailed(event -> {
+            manager.getProject().getDafnyFiles().forEach(df -> editorController.viewFile(df));
             Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).log(Level.SEVERE,
-                    "Error reloading the project: " + t.getException().getMessage(),
+                    t.getException().getMessage(),
                     t.getException());
             editorController.showException(t.getException());
             browserController.getView().setDisable(true);

commit 44245d54feee8f4870d179a4a5a85369f8484a9f

fixed bug introduced in f675e0d1f626815bc05da519d028bcd15d95ed30

modules/core/src/edu/kit/iti/algover/parser/DafnyException.java
@@ -49,7 +49,7 @@ public class DafnyException extends Exception {
         return tree;
     }
 
-    @Override
+    /*@Override
     public String getMessage() {
         String result = "";
         if (tree != null) {
@@ -57,6 +57,6 @@ public class DafnyException extends Exception {
                     + ":" + tree.getCharPositionInLine() + super.getMessage();
         }
         return result;
-    }
+    }*/
 
 }