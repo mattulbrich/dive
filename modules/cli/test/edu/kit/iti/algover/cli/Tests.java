2a230037193d2a73e9cc0986fe3b802a60a6c866
Sarah Grebing <grebing@ira.uka.de>
2019-05-23 15:14:14 +0200

    Testcases, licence and checkstyle

--- modules/cli/test/edu/kit/iti/algover/cli/Tests.java
+++ modules/cli/test/edu/kit/iti/algover/cli/Tests.java
@@ -1,10 +1,8 @@
-/*
- * This file is part of AlgoVer.
+/**
+ * This file is part of DIVE.
  *
- * Copyright (C) 2015-2018 Karlsruhe Institute of Technology
- *
+ * Copyright (C) 2015-2019 Karlsruhe Institute of Technology
  */
-
 package edu.kit.iti.algover.cli;
 
 import org.junit.runner.RunWith;
--- modules/fxgui/src/edu/kit/iti/algover/sequent/SequentController.java
+++ modules/fxgui/src/edu/kit/iti/algover/sequent/SequentController.java
@@ -1,3 +1,8 @@
+/**
+ * This file is part of DIVE.
+ *
+ * Copyright (C) 2015-2019 Karlsruhe Institute of Technology
+ */
 package edu.kit.iti.algover.sequent;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
