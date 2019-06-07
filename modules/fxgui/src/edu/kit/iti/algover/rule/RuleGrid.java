b1d301ec886b562c44955584d780400cecf6a90f
Sarah Grebing <grebing@ira.uka.de>
2019-06-07 12:48:11 +0200

    Merge

--- modules/fxgui/src/edu/kit/iti/algover/rule/RuleGrid.java
+++ modules/fxgui/src/edu/kit/iti/algover/rule/RuleGrid.java
@@ -14,10 +14,7 @@ import javafx.scene.Node;
 import javafx.scene.control.SelectionModel;
 import javafx.scene.control.SingleSelectionModel;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
