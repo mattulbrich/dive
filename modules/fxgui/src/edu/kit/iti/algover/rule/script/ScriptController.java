066b2d1193f96a5748f11d4286d7abc4f21c2c12
Mattias Ulbrich <ulbrich@kit.edu>
2019-06-27 21:19:00 +0200

    more exception output

--- modules/fxgui/src/edu/kit/iti/algover/rule/script/ScriptController.java
+++ modules/fxgui/src/edu/kit/iti/algover/rule/script/ScriptController.java
@@ -243,7 +243,11 @@ public class ScriptController implements ScriptViewListener {
         if(failException != null) {
             view.setHighlightedException(failException);
             renderException(failException);
-            Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).severe(failException.getMessage());
+            String message = failException.getMessage();
+            if (message == null || message.isEmpty()) {
+                message = failException.getClass() + " without message.";
+            }
+            Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).severe(message);
             //failException.printStackTrace();
         } else {
             Logger.getGlobal().info("Successfully ran script.");
