89147a19f0edb8f40e6cb21d9a04e48f8d3a9378
Sarah Grebing <grebing@ira.uka.de>
2019-06-27 16:16:13 +0200

    Rudimentary MethodEntity

--- modules/fxgui/src/edu/kit/iti/algover/browser/callvisualization/DafnyMethodEntity.java
+++ modules/fxgui/src/edu/kit/iti/algover/browser/callvisualization/DafnyMethodEntity.java
@@ -4,59 +4,100 @@ import edu.kit.iti.algover.dafnystructures.DafnyDecl;
 import edu.kit.iti.algover.dafnystructures.DafnyMethod;
 import edu.kit.iti.algover.parser.DafnyTree;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+import java.util.List;
 
 public class DafnyMethodEntity extends AbstractCallEntity {
+
+    private Type type;
+
+    private DafnyMethod method;
+
+    private boolean isCall;
+
+    private DafnyTree callTree;
+
+    private boolean isHidden;
+    /**
+     * Fields of a Method
+     */
+
+
+    public List<DafnyTree> mPre;
+
+    public List<DafnyTree> mPost;
+
+    public List<DafnyTree> mArguments;
+
+    public List<DafnyTree> mParams;
+
+
     public DafnyMethodEntity(DafnyMethod m, DafnyTree arg) {
-        super();
+
+        this.type = Type.METHOD;
+        this.method = m;
+        if(arg.getText().equals("CALL")){
+            isCall = true;
+        }
+        this.mPre = m.getRequiresClauses();
+        this.mParams = m.getParams();
+        this.mPost = m.getEnsuresClauses();
+        this.callTree = arg;
+        this.mArguments = callTree.getChildren().get(1).getChildren();
     }
 
     @Override
     public boolean isCall() {
-        return false;
+        return this.isCall;
     }
 
     @Override
     public boolean isHidden() {
-        return false;
+        return this.isHidden;
     }
 
     @Override
     public void setHidden(boolean hidden) {
-
+        this.isHidden = hidden;
     }
 
     @Override
     public String getHeaderText() {
-        return null;
+        return "Method "+method.getName();
     }
 
     @Override
     public int getUsageLine() {
-        return 0;
+        return callTree.getLine();
     }
 
     @Override
     public Node getNode() {
-        return null;
+        VBox vbox= new VBox();
+        Label name = new Label(getHeaderText());
+        vbox.getChildren().add(name);
+        return vbox;
     }
 
     @Override
     public Type getType() {
-        return null;
+        return this.type;
     }
 
     @Override
     public String getString() {
-        return null;
+        return getHeaderText();
     }
 
     @Override
     public String getEntityName() {
-        return null;
+        return method.getName();
     }
 
     @Override
     public DafnyDecl getEntity() {
-        return null;
+        return this.method;
     }
 }
