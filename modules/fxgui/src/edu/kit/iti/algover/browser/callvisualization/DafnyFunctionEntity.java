3643b0f7a52e5420f1f1d66f6010cb20dd42c61c
Sarah Grebing <grebing@ira.uka.de>
2019-06-28 14:35:37 +0200

    Refactoring CallVisualizationDialog

--- modules/fxgui/src/edu/kit/iti/algover/browser/callvisualization/DafnyFunctionEntity.java
+++ modules/fxgui/src/edu/kit/iti/algover/browser/callvisualization/DafnyFunctionEntity.java
@@ -2,11 +2,22 @@ package edu.kit.iti.algover.browser.callvisualization;
 
 import edu.kit.iti.algover.dafnystructures.DafnyDecl;
 import edu.kit.iti.algover.dafnystructures.DafnyFunction;
+import edu.kit.iti.algover.parser.DafnyParser;
 import edu.kit.iti.algover.parser.DafnyTree;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class DafnyFunctionEntity extends AbstractCallEntity {
@@ -52,6 +63,8 @@ public class DafnyFunctionEntity extends AbstractCallEntity {
 
     private List<DafnyTree> fParams;
 
+    private List<ParamValueObject> paramArgsList;
+
     public DafnyFunctionEntity(DafnyFunction f, DafnyTree t, HighlightingHandler listener){
 
         this.listener = listener;
@@ -67,6 +80,25 @@ public class DafnyFunctionEntity extends AbstractCallEntity {
         this.callTree = t;
         this.fArguments = callTree.getChildren().get(1).getChildren();
         this.headerText = "Function "+f.getName();
+
+
+        paramArgsList = extractParams();
+
+    }
+
+    private List<ParamValueObject> extractParams() {
+        //Name, type, value
+        List<ParamValueObject> list = new ArrayList<>();
+        Iterator<DafnyTree> argsIterator = fArguments.iterator();
+        Iterator<DafnyTree> paramsIterator = fParams.iterator();
+        while(paramsIterator.hasNext()){
+            DafnyTree params = paramsIterator.next();
+            DafnyTree arg = argsIterator.next();
+            List<DafnyTree> childrenWithType = params.getChildrenWithType(DafnyParser.TYPE);
+            assert childrenWithType.size() == 1;
+            list.add(new ParamValueObject(params.getChild(0), childrenWithType.get(0).getChild(0), arg));
+        }
+        return list;
 
     }
 
@@ -108,21 +140,51 @@ public class DafnyFunctionEntity extends AbstractCallEntity {
             listener.onRequestHighlight(callTree.getFilename(), callTree.getStartToken(), callTree.getStopToken());
         });
         vbox.getChildren().add(name);
-        vbox.getChildren().add(createArguments());
+        vbox.getChildren().add(createArgumentView());
 
         return vbox;
 
 
     }
 
-    private Node createArguments() {
-        Label name = new Label("Arguments: ");
+
+    private Node createArgumentView() {
+        Label name = new Label("Arguments: [Parameter name, Parameter type, Parameter value]");
+        VBox vbox = new VBox(name);
+        paramArgsList.forEach(paramValueObject -> {
+            HBox hbox = new HBox();
+            Label pname = new Label(paramValueObject.getName());
+            Label ptype = new Label(paramValueObject.getType());
+            Label pValue = new Label(paramValueObject.getValue());
+            hbox.getChildren().addAll(pname, ptype,pValue);
+            vbox.getChildren().add(hbox);
+        });
+
+        /*TableView<ParamValueObject> argumentTable = new TableView<ParamValueObject>();
+        argumentTable.setEditable(false);
+
+        TableColumn<ParamValueObject,String> paramName = new TableColumn<ParamValueObject,String>("Parameter name");
+        TableColumn<ParamValueObject,String> paramType = new TableColumn<ParamValueObject,String>("Parameter type");
+        TableColumn<ParamValueObject,String> argValue = new TableColumn<ParamValueObject,String>("Call value");
+        argumentTable.getColumns().add(paramName);
+        argumentTable.getColumns().add(paramType);
+        argumentTable.getColumns().add(argValue);
+
+        paramName.setCellValueFactory(new PropertyValueFactory<ParamValueObject,String>("name"));
+        paramType.setCellValueFactory(new PropertyValueFactory<ParamValueObject,String>("type"));
+        argValue.setCellValueFactory(new PropertyValueFactory<ParamValueObject,String>("value"));
+        ObservableList<ParamValueObject> list = FXCollections.observableArrayList(this.paramArgsList);
+
+        argumentTable.setItems(list);
+
+
         name.setOnMouseClicked(event -> {
             fArguments.size();
             fParams.size();
         });
-        fArguments.size();
-        return name;
+        fArguments.size();*/
+
+        return vbox;
     }
 
     @Override
@@ -135,5 +197,38 @@ public class DafnyFunctionEntity extends AbstractCallEntity {
     @Override
     public String getEntityName() {
         return this.getEntity().getName();
+    }
+
+    public class ParamValueObject{
+        DafnyTree typeO;
+        DafnyTree nameO;
+        DafnyTree valueO;
+
+        private final SimpleStringProperty type;
+        private final SimpleStringProperty name;
+        private final SimpleStringProperty value;
+
+        public ParamValueObject(DafnyTree name, DafnyTree type, DafnyTree value) {
+            this.typeO = type;
+            this.nameO = name;
+            this.valueO = value;
+            this.type = new SimpleStringProperty(typeO.getText());
+            this.name =  new SimpleStringProperty(nameO.getText());
+            //todo abstieg im Baum
+            this.value = new SimpleStringProperty(valueO.getText());
+
+        }
+
+        public String getType() {
+            return type.get();
+        }
+
+        public String getName() {
+            return name.get();
+        }
+
+        public String getValue() {
+            return value.get();
+        }
     }
 }
