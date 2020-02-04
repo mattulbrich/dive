e14f68ed51b071b64e496fef57370d76dec3df69
Mattias Ulbrich <ulbrich@kit.edu>
2020-02-04 15:03:25 +0100

    completing function translation to boogie.

--- modules/dafny/src/edu/kit/iti/algover/boogie/BoogieFunctionDefinitionVisitor.java
+++ modules/dafny/src/edu/kit/iti/algover/boogie/BoogieFunctionDefinitionVisitor.java
@@ -24,35 +24,56 @@ import edu.kit.iti.algover.term.builder.ReplacementVisitor;
 import edu.kit.iti.algover.term.builder.TermBuildException;
 import edu.kit.iti.algover.term.builder.TermBuilder;
 import edu.kit.iti.algover.term.builder.TreeTermTranslator;
-import edu.kit.iti.algover.util.ImmutableLinearSet;
-import edu.kit.iti.algover.util.Util;
+import nonnull.NonNull;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.IdentityHashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Queue;
 import java.util.Set;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
+ * This class can be used to visit terms. It then identifies called functions
+ * inside. If it encounters a not yet observed function it adds it to a to-do
+ * list. If it encounters an already observed function in the same strongly
+ * connected call component, it replaces it by a "$limited" derivate.
+ *
+ * Axioms for all encountered functions are added, their definitions are
+ * visited as well to obtain a more precise translation.
+ *
  * Requires that {@link edu.kit.iti.algover.dafnystructures.TarjansAlgorithm} has
- * been executed as this adds the infomration about the strongly connected
+ * been executed as this adds the information about the strongly connected
  * components.
+ *
+ * @author mattias ulbrich
  */
 public class BoogieFunctionDefinitionVisitor extends ReplacementVisitor<Void> {
 
-    private Map<DafnyFunctionSymbol, FunctionSymbol> introducedLimitFunctions = new LinkedHashMap<>();
-    private Queue<DafnyFunctionSymbol> todo = new LinkedList<>();
-    private Set<DafnyFunctionSymbol> done = new HashSet<>();
-    private DafnyFunctionSymbol currentSymbol;
+    /**
+     * collection of already introduced $limited variants.
+     */
+    private Map<DafnyFunctionSymbol, FunctionSymbol> introducedLimitFunctions =
+            new LinkedHashMap<>();
 
-    public BoogieFunctionDefinitionVisitor() {
-    }
+    /**
+     * encountered symbols that should be axiomatised
+     */
+    private Queue<DafnyFunctionSymbol> todo = new LinkedList<>();
+
+    /**
+     * encountered symbols that have already been axiomatised
+     */
+    private Set<DafnyFunctionSymbol> done = new HashSet<>();
+
+    /**
+     * the symbol in whose definition we currently are.
+     */
+    private DafnyFunctionSymbol currentSymbol;
 
     @Override
     public Term visit(ApplTerm term, Void arg) throws TermBuildException {
@@ -64,12 +85,18 @@ public class BoogieFunctionDefinitionVisitor extends ReplacementVisitor<Void> {
             DafnyFunctionSymbol dfs = (DafnyFunctionSymbol) fs;
             if(currentSymbol != null && done.contains(dfs) &&
                     getSCC(dfs).equals(getSCC(currentSymbol))) {
+
+                // Interesting case: symbol already seen and in same scc -->
+                // replace by $limited version.
+
                 FunctionSymbol replacement = introducedLimitFunctions.get(dfs);
                 if (replacement == null) {
-                    replacement = new FunctionSymbol(dfs.getName() + "$limited", dfs.getResultSort(), dfs.getArgumentSorts());
+                    replacement = new FunctionSymbol(dfs.getName() + "$limited",
+                            dfs.getResultSort(), dfs.getArgumentSorts());
                     introducedLimitFunctions.put(dfs, replacement);
                 }
-                List<Term> subterms = visited != null ? visited.getSubterms() : term.getSubterms();
+                List<Term> subterms =
+                        visited != null ? visited.getSubterms() : term.getSubterms();
                 return new ApplTerm(replacement, subterms);
             }
 
@@ -81,19 +108,32 @@ public class BoogieFunctionDefinitionVisitor extends ReplacementVisitor<Void> {
         return visited;
     }
 
+    /*
+     * read the SCC from the place where TarjansAlgorithm has put it.
+     */
     private static String getSCC(DafnyFunctionSymbol dfs) {
         DafnyTree declRef = dfs.getOrigin().getRepresentation().getExpressionType();
         assert declRef.getType() == TarjansAlgorithm.CALLGRAPH_SCC;
         return declRef.toString();
     }
 
-
-    public void findFunctionDefinitions(SymbolTable symbols, BoogieVisitor visitor) throws TermBuildException {
+    /**
+     * Iterates over the function symbols collected during earlier visitations.
+     *
+     * Adds according axioms to the axiom set of {@code visitor}.
+     *
+     * The definitions are themselves visited possible adding further symbols
+     * to the collection.
+     *
+     * @param symbols the table to take symbols from, not changed.
+     * @param visitor used for producing and stoprin axioms.
+     * @throws TermBuildException
+     */
+    public void findFunctionDefinitions(@NonNull SymbolTable symbols, @NonNull BoogieVisitor visitor) throws TermBuildException {
         Function<Term, Term> oldTrigFunc = visitor.getTriggerFunction();
         while(!todo.isEmpty()) {
             currentSymbol = todo.remove();
             done.add(currentSymbol);
-            DafnyFunction function = currentSymbol.getOrigin();
             Term axiom = makeAxiom(currentSymbol, symbols);
             Term equality = stripQuantifiers(axiom);
             visitor.setTriggerFunction(t ->
@@ -103,8 +143,55 @@ public class BoogieFunctionDefinitionVisitor extends ReplacementVisitor<Void> {
             visitor.getAxioms().add("axiom " + axiom.accept(visitor, null) + ";");
         }
         visitor.setTriggerFunction(oldTrigFunc);
+
+        addLimitationAxioms(visitor, symbols);
+
     }
 
+    /*
+     * Add axioms for the limited functions of the form
+     *    forall ... :: { f(x) } f(x) = f$limit(x)
+     * to make the symbols "more the same".
+     */
+    private void addLimitationAxioms(BoogieVisitor visitor, SymbolTable symbols) throws TermBuildException {
+        Function<Term, Term> oldTrigFunc = visitor.getTriggerFunction();
+        for (Entry<DafnyFunctionSymbol, FunctionSymbol> en : introducedLimitFunctions.entrySet()) {
+            FunctionSymbol f1 = en.getKey();
+            FunctionSymbol f2 = en.getValue();
+            Term axiom;
+            axiom = makeLimitAxiom(f1, f2, symbols);
+            Term equality = stripQuantifiers(axiom);
+            visitor.setTriggerFunction(t ->
+                    t == equality ? t.getTerm(0) :
+                            oldTrigFunc != null ? oldTrigFunc.apply(t) : null);
+            visitor.getAxioms().add("axiom " + axiom.accept(visitor, null) + ";");
+        }
+        visitor.setTriggerFunction(oldTrigFunc);
+    }
+
+    /*
+     * make forall ... :: f1(x) = f2(x)
+     */
+    private Term makeLimitAxiom(FunctionSymbol f1, FunctionSymbol f2, SymbolTable symbols) throws TermBuildException {
+        TermBuilder tb = new TermBuilder(symbols);
+        List<VariableTerm> boundVars = new ArrayList<>();
+        int i = 0;
+        for (Sort argumentSort : f1.getArgumentSorts()) {
+            boundVars.add(new VariableTerm("v" + i, argumentSort));
+            i++;
+        }
+        ApplTerm appl1 = new ApplTerm(f1, boundVars);
+        ApplTerm appl2 = new ApplTerm(f2, boundVars);
+        Term result = tb.eq(appl1, appl2);
+        for (VariableTerm boundVar : boundVars) {
+            result = tb.forall(boundVar, result);
+        }
+        return result;
+    }
+
+    /*
+     * get the innermost matrix of possibly nested quantifiers.
+     */
     private Term stripQuantifiers(Term term) {
         while (term instanceof QuantTerm) {
             term = term.getTerm(0);
@@ -114,7 +201,9 @@ public class BoogieFunctionDefinitionVisitor extends ReplacementVisitor<Void> {
 
     /*
      * The result is of the form
-     *    f(var0, var1, var2) = ...
+     *      forall var0. forall var1. forall var2. f(var0, var1, var2) = term
+     *
+     * term is visited as well.
      */
     private Term makeAxiom(DafnyFunctionSymbol dfs, SymbolTable symbols) throws TermBuildException {
 
@@ -150,13 +239,24 @@ public class BoogieFunctionDefinitionVisitor extends ReplacementVisitor<Void> {
         translation = tb.eq(app, translation);
 
         for (VariableTerm var : boundVars) {
-            translation = new QuantTerm(Quantifier.FORALL, var, translation);
+            translation = tb.forall(var, translation);
         }
 
         return translation;
 
     }
 
+    /**
+     * Visit the argument term.
+     *
+     * Unlike the visitor methods of this class, this version does not return
+     * null of no changes have occurred but the original term.
+     *
+     * @param term term to visit
+     * @return visited version, possibly with some function invocations replaced
+     * by limited versions, not null
+     * @throws TermBuildException if construction fails.
+     */
     public Term collectAndMask(Term term) throws TermBuildException {
         Term result = term.accept(this, null);
         if (result == null) {
