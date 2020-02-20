4705a35fa780e95246eca7d9b770a99a30442946
SpringVaS <j1gITh5b>
2020-02-20 11:19:37 +0100

    Started Timeline refactoring

--- modules/fxgui/src/edu/kit/iti/algover/timeline/TimelineLayout.java
+++ modules/fxgui/src/edu/kit/iti/algover/timeline/TimelineLayout.java
@@ -9,11 +9,17 @@ import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
+import javafx.beans.Observable;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.beans.value.WritableValue;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.SplitPane;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 import org.controlsfx.control.HiddenSidesPane;
 
@@ -29,11 +35,13 @@ public class TimelineLayout extends HiddenSidesPane {
     private static final double HOVER_AREA = 20;
 
     private final List<Node> nodes;
+    private final Pane contentPane;
     private final SplitPane splitPane;
     private final GoLeftArrow goLeft;
     private final GoRightArrow goRight;
 
-    private int framePosition = 0;
+
+    private SimpleIntegerProperty framePosition;
 
     public TimelineLayout(Node... nodes) {
         if (nodes.length < 2) {
@@ -47,10 +55,22 @@ public class TimelineLayout extends HiddenSidesPane {
         this.splitPane = new SplitPane();
         this.splitPane.setPadding(new Insets(0, HOVER_AREA, 0, HOVER_AREA));
 
-        setContent(splitPane);
+        contentPane = new Pane();
+        contentPane.getChildren().add(splitPane);
+
+        setContent(contentPane);
         setAnimationDelay(Duration.ZERO);
         setAnimationDuration(Duration.millis(100));
         setTriggerDistance(HOVER_AREA);
+
+        framePosition = new SimpleIntegerProperty(0);
+
+        framePosition.addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
+
+            }
+        });
 
 
         setOnKeyPressed(event -> {
@@ -62,33 +82,46 @@ public class TimelineLayout extends HiddenSidesPane {
                     moveFrameLeft();
                     event.consume();
                 } else if (event.getCode() == KeyCode.DIGIT1) {
-                    goToView(0);
+                    framePosition.set(0);
                 } else if (event.getCode() == KeyCode.DIGIT2) {
-                    goToView(1);
+                    framePosition.set(1);
                 } else if (event.getCode() == KeyCode.DIGIT3) {
-                    goToView(2);
+                    framePosition.set(2);
                 }
             }
 
         });
+        setUpFrame();
         updateFrame();
+
     }
 
     public void setDividerPosition(double position) {
-        splitPane.setDividerPositions(position);
+        ObservableList<SplitPane.Divider> dividers = splitPane.getDividers();
+
+        for (int i = 0; i < dividers.size(); i++) {
+            dividers.get(i).setPosition((i % 2 == 0) ? position : (1 - position));
+        }
     }
 
+    private void setUpFrame() {
+        splitPane.getItems().setAll(nodes);
+        splitPane.prefWidthProperty().bind(contentPane.widthProperty().multiply(2));
+        splitPane.prefHeightProperty().bind(contentPane.heightProperty());
+    }
+
+
     private void updateFrame() {
-        assert framePosition < nodes.size() - 1;
+        assert framePosition.get() < nodes.size() - 1;
 
-        splitPane.getItems().setAll(nodes.get(framePosition), nodes.get(framePosition + 1));
+        //splitPane.getItems().setAll(nodes.get(framePosition), nodes.get(framePosition + 1));
 
-        if (framePosition == 0) {
+        if (framePosition.isEqualTo(0).get()) {
             setLeft(null);
         } else {
             setLeft(goLeft);
         }
-        if (framePosition == nodes.size() - 2) {
+        if (framePosition.isEqualTo(nodes.size() - 2).get()) {
             setRight(null);
         } else {
             setRight(goRight);
@@ -103,40 +136,40 @@ public class TimelineLayout extends HiddenSidesPane {
 
     public boolean moveFrameRight() {
 
-        if (framePosition >= nodes.size() - 2) {
+        if (framePosition.greaterThan(nodes.size() - 2).get()) {
             return false;
         }
 
-        Node leftNode = nodes.get(framePosition);
+        Node leftNode = nodes.get(framePosition.get());
 
         double divider = splitPane.getDividerPositions()[0];
         double leftNodeWidth = leftNode.getBoundsInParent().getWidth();
 
-        framePosition++;
+        framePosition.set(framePosition.get() + 1);
         updateFrame();
 
-        setDividerPosition(1 - divider);
+        //setDividerPosition(1 - divider);
 
-        splitPane.setTranslateX(leftNodeWidth);
-        animate(splitPane.translateXProperty(), 0);
+        splitPane.setTranslateX(0);
+        animate(splitPane.translateXProperty(), -2000);
 
         return true;
     }
 
     public boolean moveFrameLeft() {
-        if (framePosition < 1) {
+        if (framePosition.get() < 1) {
             return false;
         }
 
-        Node rightNode = nodes.get(framePosition + 1);
+        Node rightNode = nodes.get(framePosition.get() + 1);
 
         double divider = splitPane.getDividerPositions()[0];
         double rightNodeWidth = rightNode.getBoundsInParent().getWidth();
 
-        framePosition--;
+        framePosition.set(framePosition.get() - 1);
         updateFrame();
 
-        setDividerPosition(1 - divider);
+        //setDividerPosition(1 - divider);
 
         splitPane.setTranslateX(-rightNodeWidth);
         animate(splitPane.translateXProperty(), 0);
@@ -144,32 +177,6 @@ public class TimelineLayout extends HiddenSidesPane {
         return true;
     }
 
-    /**
-     * Move to specified View. Now implement as several calls to
-     * move in the right direction.
-     * @param viewIndex Index of target view
-     * @return True if view changed.
-     */
-    private boolean goToView(int viewIndex) {
-        int delta = viewIndex - framePosition;
-
-        if (delta == 0) {
-            return false;
-        }
-
-        if (delta > 0) { // move right
-            while (delta > 0) {
-                moveFrameRight();
-                delta = viewIndex - framePosition;
-            }
-        } else {
-            while (delta < 0) {
-                moveFrameLeft();
-                delta = viewIndex - framePosition;
-            }
-        }
-        return true;
-    }
 
     private <T> void animate(WritableValue<T> value, T target) {
         KeyValue xkeyvalue = new KeyValue(value, target);
