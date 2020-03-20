4cc2bb1586d62a074f8fe991cbd8681dfccc322b
Valentin Springsklee <valentin.springsklee@student.kit.edu>
2020-03-20 19:12:46 +0100

    Added horizontal scroll bar to sequent.

--- modules/fxgui/src/edu/kit/iti/algover/sequent/BasicFormulaView.java
+++ modules/fxgui/src/edu/kit/iti/algover/sequent/BasicFormulaView.java
@@ -295,6 +295,8 @@ public class BasicFormulaView extends CodeArea {
         // This is a hack, but it seems to be impossible without it...
         Bounds bounds = TextUtil.computeTextBounds(text, getStyleClass(), getStylesheets(), fontsizeProperty.get());
 
+        this.setPrefWidth(bounds.getWidth());
+
         final double safetyPadding = 1.1; // 10%, this is such a hack ... :(
 
         return safetyPadding * (bounds.getHeight()
