fa1998bf89ae8ac9e70ca2eb3ac7493897a335f8
Mattias Ulbrich <ulbrich@kit.edu>
2020-05-01 11:27:11 +0200

    adding javadoc

--- modules/fxgui/src/edu/kit/iti/algover/script/ProofHTML.java
+++ modules/fxgui/src/edu/kit/iti/algover/script/ProofHTML.java
@@ -11,10 +11,27 @@ import static j2html.TagCreator.*;
 
 import java.io.IOException;
 
-public class ProofHTML {
+/*
+ * Container for the static method toHTML that translates a script to a htmlized form.
+ *
+ * TODO Add this with a conducted proof to learn about errors and open goals.
+ *
+ * @author Mattias Ulbrich
+ */
+public final class ProofHTML {
 
-    private static String HEAD = readHead();
+    private ProofHTML() {
+        throw new Error("not to be instantiated");
+    }
 
+    /**
+     * The CSS styles as {@code <style> ... </style>} string.
+     */
+    private final static String HEAD = readHead();
+
+    /*
+     * read the styles from a resource.
+     */
     private static String readHead() {
         try {
             return Util.streamToString(ProofHTML.class.getResourceAsStream("css.css"));
@@ -25,28 +42,33 @@ public class ProofHTML {
     }
 
     public static String toHTML(Script script) {
-        ProofHTML xml = new ProofHTML();
-        return html(head(style(HEAD)), body(xml.toDiv(script))).render();
+        return html(head(style(HEAD)), body(toDiv(script))).render();
     }
 
-    private Tag toDiv(Script proofScript) {
-        return div(attrs(".script"), each(proofScript.getStatements(), this::toTag));
+    /*
+     * an entire script as HTML
+     */
+    private static Tag<?> toDiv(Script proofScript) {
+        return div(attrs(".script"), each(proofScript.getStatements(), ProofHTML::toTag));
     }
 
-    private Tag toTag(Statement statement) {
-        return statement.visit(this::commandToTag, this::casesToTag);
+    /*
+     * statements can be commands or cases. Make case distinction here.
+     */
+    private static Tag<?> toTag(Statement statement) {
+        return statement.visit(ProofHTML::commandToTag, ProofHTML::casesToTag);
     }
 
-    private Tag casesToTag(Cases cases) {
+    private static Tag<?> casesToTag(Cases cases) {
         return div(attrs(".cases"), span(attrs(".casesName"), "cases"),
             each(cases.getCases(), c ->
                     div(attrs(".case"), span(attrs(".caseLabel"),
                             "case " + c.getLabel().getText() + ":"),
                             div(attrs(".block"),
-                                    each(c.getStatements(), this::toTag)))));
+                                    each(c.getStatements(), ProofHTML::toTag)))));
     }
 
-    private Tag commandToTag(Command command) {
+    private static Tag<?> commandToTag(Command command) {
         return div(attrs(".call"),
                 span(attrs(".ruleName"), command.getCommand().getText()),
                 each(command.getParameters(), p ->
