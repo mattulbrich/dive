dcb5536ec1d606e7ec84992347af682b5028b727
Mattias Ulbrich <ulbrich@kit.edu>
2020-06-02 11:20:47 +0200

    checkstyle in swing

--- modules/gui/src/edu/kit/iti/algover/swing/util/Settings.java
+++ modules/gui/src/edu/kit/iti/algover/swing/util/Settings.java
@@ -18,18 +18,17 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
-import java.util.logging.Logger;
 
 import nonnull.NonNull;
 
 /**
  * Settings are "pimped {@link Properties}".
- * 
+ *
  * Only they provide typed getter-methods.
- * 
+ *
  * If a property is looked up, the order is the following:
- * 
- * Defaults are read from the file <code>Settings_default.properties
+ *
+ * Defaults are read from the file <code>Settings_default.properties</code>.
  * <ol>
  * <li>the settings stored in the Settings_default.properties" file in the
  * resources
@@ -37,26 +36,33 @@ import nonnull.NonNull;
  * commandline options!)
  * <li>the settings loaded/set by the program.
  * </ol>
- * 
+ *
  * Settings are used for settings on a per-user level. Other info should be
  * stored in szenario files.
- * 
+ *
  * Store results in caches for the conversion may be costly.
- * 
+ *
  * For convenience also store all keys in here.
- * 
+ *
  * This is a singleton.
- * 
+ *
  * @author mattze
  */
 public class Settings {
 
-    public static final String SYSTEM_DIRECTORY_KEY = "pseudo.sysDir";
-
+    /**
+     * To avoid producing object multiple times, a cache saves them for later lookup.
+     */
     private Map<String, Object> cache = new HashMap<String, Object>();
-    
+
+    /**
+     * The properties that back this object up.
+     */
     private Properties contents;
 
+    /**
+     * The singleton instance.
+     */
     private static Settings theInstance = new Settings();
 
     /**
@@ -73,16 +79,29 @@ public class Settings {
             contents.putAll(System.getProperties());
         } catch (Exception ex) {
             throw new RuntimeException("Cannot initialise Settings", ex);
-        } 
+        }
     }
 
+    /**
+     * Get the singleton instance of the Settings class.
+     * <p>
+     * The object is created at class loading time.
+     * </p>
+     * @return a non-null instance of Settings. The same on any invocation.
+     */
     public static Settings getInstance() {
         return theInstance;
     }
 
-    public synchronized Object put(String key, String value) {
-//        if (logger.isLoggable(Level.CONFIG))
-//            logger.config("Settings updated: " + key + " -> " + value);
+    /**
+     * store a single value into the settings.
+     *
+     * It is stored in the underlying properties.
+     * @param key the key store under
+     * @param value the string value to store
+     * @return the previous value associated with key, or null if there was no mapping for key.
+     */
+    public synchronized Object put(@NonNull String key, @NonNull String value) {
         return contents.put(key, value);
     }
 
@@ -97,7 +116,7 @@ public class Settings {
             return null;
         }
     }
-    
+
     private void putCache(String key, Object value) {
         assert value != null;
         cache.put(key, value);
@@ -106,7 +125,7 @@ public class Settings {
     /**
      * lookup a value in the properties and use its value as a class name and
      * create a new instance of that class
-     * 
+     *
      * @param key
      *            key for the classname
      * @return new instance of the class of that class name
@@ -120,29 +139,24 @@ public class Settings {
                 clss = Class.forName(value);
                 putCache(key, clss);
             }
-            return clss.newInstance();
-        } catch (ClassNotFoundException e) {
-            throw new SettingsException("Cannot create class for key " + key, e);
-        } catch (InstantiationException e) {
-            throw new SettingsException("Cannot create class for key " + key, e);
-        } catch (IllegalAccessException e) {
+            return clss.getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
             throw new SettingsException("Cannot create class for key " + key, e);
         }
     }
 
     /**
      * Lookup an integer value in the properties.
-     * 
+     *
      * <p>
      * The number can be specified as decimal constant, octal or hexadecimal. It
      * accepts the same input as {@link Integer#decode(String)}, see there for
      * details.
-     * 
+     *
      * @see Integer#decode(String)
-     * @param key
-     *                key to look up
+     * @param key      key to look up
      * @return integer value of the key's value
-     * 
+     *
      * @throws SettingsException
      *                 if the value does not represent an integer or
      *                 if the key has not been defined in these settings.
@@ -155,7 +169,26 @@ public class Settings {
             throw new SettingsException("Cannot create integer for key " + key, e);
         }
     }
-    
+
+    /**
+     * Lookup an integer value in the properties.
+     *
+     * <p>
+     * The number can be specified as decimal constant, octal or hexadecimal. It
+     * accepts the same input as {@link Integer#decode(String)}, see there for
+     * details.
+     *
+     * <p>The method returns a default value if the key is not present.</p>
+     *
+     * @see Integer#decode(String)
+     * @param key      key to look up
+     * @param defaultValue the value to return on case that key is not present in the settings
+     * @return integer value of the key's value
+     *
+     * @throws SettingsException
+     *                 if the value does not represent an integer or
+     *                 if the key has not been defined in these settings.
+     */
     public int getInteger(String key, int defaultValue) {
         try {
             return getInteger(key);
@@ -173,31 +206,31 @@ public class Settings {
      * The method throws a
      * {@link SettingsException} if the key is not present in any property
      * list.
-     * 
+     *
      * @param key
      *                the property key.
-     *                
+     *
      * @return the value in this property list with the specified key value.
-     * 
+     *
      * @throws SettingsException
      *                 if key has not been defined
      */
     public String getProperty(@NonNull String key) throws SettingsException {
         if (!contents.containsKey(key))
             throw new SettingsException("No value for key " + key);
-        
+
         return contents.getProperty(key);
     }
-    
+
     /**
      * Searches for the property with the specified key in this property list.
      * The method returns a default value if the key is not present
-     * 
+     *
      * @param key
      *                the property key.
      * @param defaultValue
      *                the default value to return if the key is not present.
-     *                
+     *
      * @return the value in this property list with the specified key value.
      */
     public String getProperty(String key, String defaultValue) {
@@ -210,11 +243,38 @@ public class Settings {
             return defaultValue;
         }
     }
-    
+
+    /**
+     * Searches for the property with the specified key in this property list.
+     *
+     * The result is expanded before return. This means that any expression
+     * {@code ${name}} is resolved by looking up name in these settings and
+     * inlining their string.
+     *
+     * @param key
+     *                the property key.
+     *
+     * @return the value in this property list with the specified key value.
+     */
     public String getExpandedProperty(String key) throws SettingsException {
         return expandString(getProperty(key));
     }
-    
+
+    /**
+     * Searches for the property with the specified key in this property list.
+     * The method returns a default value if the key is not present.
+     *
+     * The result is expanded before return. This means that any expression
+     * {@code ${name}} is resolved by looking up name in these settings and
+     * inlining the string.
+     *
+     * @param key
+     *                the property key.
+     * @param defaultValue
+     *                the default value to return if the key is not present.
+     *
+     * @return the value in this property list with the specified key value.
+     */
     public String getExpandedProperty(String key, String defaultValue) {
         try {
             String value = getProperty(key);
@@ -233,7 +293,7 @@ public class Settings {
     private String expandString(String value) {
         StringBuilder result = new StringBuilder();
         int keyStart = -1;
-        
+
         for(int i = 0; i < value.length(); i++) {
             char c = value.charAt(i);
             switch (c) {
@@ -247,7 +307,7 @@ public class Settings {
                     }
                 }
                 break;
-                
+
             case '}':
                 if(keyStart == -1) {
                     result.append(c);
@@ -261,7 +321,7 @@ public class Settings {
                         Log.log(Log.WARNING, "Cannot expand ${" + key + "}, no such key defined");
                         expansion = "";
                     }
-                    
+
                     result.append(expansion);
                     keyStart = -1;
                 }
@@ -274,11 +334,20 @@ public class Settings {
                 break;
             }
         }
-        
+
         return result.toString();
     }
 
-    // / @todo DOC
+    /**
+     * Searches for the property with the specified key in this property list.
+     *
+     * The value is obtained from the string at the key by splitting it at commas.
+     *
+     * @param key
+     *                the property key.
+     *
+     * @return the value in this property list with the specified key value.
+     */
     public String[] getStrings(String key) throws SettingsException {
         String value = getProperty(key);
         return value.split(", *");
@@ -287,7 +356,7 @@ public class Settings {
     /**
      * return the boolean property that is stored in key.
      * If there is no such property false is returned.
-     * 
+     *
      * @param key the key to lookup
      * @return true iff key stored and has the value "true"
      */
@@ -298,7 +367,7 @@ public class Settings {
     /**
      * return the boolean property that is stored in key. If there is no such
      * property defValue is returned.
-     * 
+     *
      * @param key
      *            the key to lookup
      * @param defaultValue
@@ -328,11 +397,11 @@ public class Settings {
 
     /**
      * Use the {@link ColorResolver} to resolve a color name or id.
-     * 
+     *
      * @param key
      *            key to look up
      * @return a Color object
-     * @throws SettingsException 
+     * @throws SettingsException
      */
     public @NonNull Color getColor(@NonNull String key) throws SettingsException {
         Color color = getCached(key, Color.class);
@@ -347,18 +416,18 @@ public class Settings {
 
         return color;
     }
-    
-    
+
+
     /**
      * Use the {@link ColorResolver} to resolve a color name or id.
-     * 
+     *
      * Return a default value in case the lookup fails.
-     * 
+     *
      * @param key
      *                key to look up
      * @param defaultColor
      *                return this value if the lookup fails.
-     * 
+     *
      * @return a Color object
      */
     public Color getColor(String key, Color defaultColor) {
@@ -375,17 +444,17 @@ public class Settings {
 
     /**
      * get a Font object for a key.
-     * 
+     *
      * "Fontname, style, size" is the format. "Arial, PLAIN, 12" for instance.
-     * 
+     *
      * Styles are: PLAIN, BOLD, ITALIC, BOLD+ITALIC. (not ITALIC+BOLD)
-     * 
+     *
      * @param key
      *                key to look up
-     *                
+     *
      * @return a Font object
-     * 
-     * @throws SettingsException 
+     *
+     * @throws SettingsException
      *                 if there is no value for key or
      *                 if the font size is not correctly defined or
      *                 if the style string is illegal or
@@ -398,7 +467,7 @@ public class Settings {
             try {
                 if(strings.length != 3)
                     throw new IllegalArgumentException("Font descriptions must have exactly 3 parts");
-                
+
                 String fontname = strings[0];
                 int style = decodeStyle(strings[1]);
                 int size = Integer.decode(strings[2]);
@@ -413,21 +482,21 @@ public class Settings {
 
         return font;
     }
-    
+
     /**
      * get a Font object for a key. Return a default font if the indicated
      * key is missing or the description is invalid.
-     * 
+     *
      * "Fontname, style, size" is the format. "Arial, PLAIN, 12" for instance.
-     * 
+     *
      * Styles are: PLAIN, BOLD, ITALIC, BOLD+ITALIC. (not ITALIC+BOLD)
-     * 
+     *
      * @param key
      *            key to look up
      * @param defaultFont
      *            the font object to return in case of an invalid description
      *            or an error.
-     *            
+     *
      * @return a Font object
      */
     public Font getFont(String key, Font defaultFont) {
@@ -457,7 +526,7 @@ public class Settings {
     /**
      * once the command line args have been parsed we can now load the settings
      * to the defaults - not overwriting set arguments
-     * 
+     *
      * @throws IOException
      *             file not found or reading error
      */
@@ -468,7 +537,7 @@ public class Settings {
 //            if(f.canRead()) {
 ////            logger.fine("Load settings from system directory: " + f);
 //                FileInputStream fileInputStream = new FileInputStream(f);
-//                
+//
 //                try {
 //                    defaults.load(fileInputStream);
 //                } finally {
@@ -481,10 +550,10 @@ public class Settings {
 
     /**
      * Load properties from a file into the settings.
-     * 
+     *
      * <p>
      * The filename is obtained by querying the settings for the argument key.
-     * 
+     *
      * @throws IOException
      *                 if something goes wrong while reading properties or
      *                 resolving names.
@@ -498,7 +567,7 @@ public class Settings {
             ex.initCause(e);
             throw ex;
         }
-        
+
         FileInputStream fileInputStream = new FileInputStream(fileName);
         try {
             contents.load(fileInputStream);
@@ -508,13 +577,19 @@ public class Settings {
         }
     }
 
+    /**
+     * Remove all entries from this settings object,
+     */
     public void clear() {
         contents.clear();
     }
 
-    public void putAll(Properties properties) {
+    /**
+     * Add all entries from the argument to this object.
+     *
+     * @param properties another collection of properties
+     */
+    public void putAll(@NonNull Properties properties) {
         contents.putAll(properties);
     }
-
-   
 }
